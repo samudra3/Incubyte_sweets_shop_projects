e5d2de703ef5c627a1df03b8a6360cf157e0f532
Sahil <sahilsingh@gmai.com>
2025-12-14 01:44:07 +0530

    feat(auth): implement JWT authentication and admin authorization middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const authRoutes = require("./modules/auth/auth.routes");
+const { authenticate } = require("./middlewares/auth.middleware");
 const app = express();
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+
+//Temporary route for middleware Testing
+app.get("/api/protected-test", authenticate, (req, res) => {
+      res.status(200).json({ message: "Access granted" });
+    });
+
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Sweet Shop API is running" });
 });
---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+function authenticate(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // { userId, role }
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
+
+function authorizeAdmin(req, res, next) {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Admin access required" });
+  }
+  next();
+}
+
+module.exports = { authenticate, authorizeAdmin };
